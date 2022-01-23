c129c450ed025f2938056f247904edb61af157d3
anil antony <anil@jecc.ac.in>
2022-01-23 15:46:06 +0530

    Update File.java

--- File.java
+++ File.java
@@ -6,9 +6,17 @@ class File
     {
       try
       {
-        FileReader fr=new FileReader(testing.txt);
+        FileReader fr=new FileReader("testing.txt");
+        FileWriter fw=new FileWriter("copy.txt");
         BufferedReader br=new BufferedReader(fr);
-        System.out.println(br.readLine());
+        String str;
+        while((str=br.readLine())!=null)
+        {   
+            System.out.println(str);
+            fw.write(str);
+        }
+        fr.close();
+        fw.close();
       }
       catch(IOException e)
       {
