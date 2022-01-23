40d19983414547a67e3d5e1819110cf1e1d01937
anil antony <anil@jecc.ac.in>
2022-01-23 19:05:03 +0530

    Update FileExc.java

--- FileExc.java
+++ FileExc.java
@@ -1,1 +1,29 @@
 // Java Program that read from a file and write to file by handling all file related exceptions
+import java.io.*;
+class FileExc
+  {
+    public static void main(String args[])
+    {
+      try
+      {
+        FileReader fin=new FileReader("testing.txt");
+        FileWriter fout=new FileWriter("copy.txt");
+        int i;
+        while((i=fin.read())!=-1)
+        {
+          System.out.println((char) i);
+          fout.write(i);
+        }
+        fin.close();
+        fout.close();
+      }
+      catch(FileNotFoundException e)
+      {
+        System.out.println(e.getMessage());
+      }
+      catch(IOException e)
+      {
+        System.out.println(e.getMessage());
+      }
+     } 
+   }
